efd391732667ad60c341442baac8b6bc2b6e9f16
Anwesh43 <anweshthecool0@gmail.com>
2017-06-13 17:32:54 +0530

    adding animation handler

--- border-fill-component.js
+++ border-fill-component.js
@@ -5,6 +5,25 @@ class BorderFillComponent extends HTMLElement{
         this.img = document.createElement('img')
         shadow.appendChild(this.img)
         this.src = this.getAttribute('src')
+        this.color = this.getAttribute('color') || '#00838F'
+        this.animationHandler = new AnimationHandler(this)
+    }
+    update() {
+        if(this.index != undefined) {
+            this.index+=(this.points.length)/15
+        }
+        else {
+            console.log("not updating")
+
+        }
+    }
+    stopped() {
+        const condition =  this.index > this.points.length
+        if(condition == true) {
+            this.index = this.points.length -1
+            this.render()
+        }
+        return condition
     }
     render() {
         const canvas = document.createElement('canvas')
@@ -19,13 +38,25 @@ class BorderFillComponent extends HTMLElement{
             BorderIdentifyingUtil.getBorder(context,this.points,w,h)
             console.log(this.points)
         }
+        context.lineWidth = w/80
+        context.fillStyle = this.color
+        context.beginPath()
+        for(var i=0;i<this.index;i++) {
+            const point = this.points[i]
+            context.beginPath()
+            context.arc(point.x,point.y,context.lineWidth/3,0,2*Math.PI)
+            context.fill()
+        }
+
         this.img.src = canvas.toDataURL()
+        console.log(this.index-this.points.length)
     }
     connectedCallback() {
         this.image = new Image()
         this.image.src = this.src
         this.image.onload = () => {
             this.render()
+            this.animationHandler.start()
         }
     }
 }
@@ -38,7 +69,7 @@ class BorderIdentifyingUtil {
         for(var i=0;i<data.length;i+=4) {
             const alpha = data[i+3]
             const j = i/4
-            const x = j%w,y = Math.floor(i/w)
+            const x = j%w,y = Math.floor(j/w)
             if(alpha == 0) {
                 //console.log(0)
                 opaquePoints++
@@ -48,7 +79,7 @@ class BorderIdentifyingUtil {
             for(var a = -1;a<=1;a++) {
                 for(var b = -1;b<=1;b++) {
                     const newX = x+b,newY = y+a
-                    if(newX>=0 && newX<w && newX >=0 && newY<h) {
+                    if(newX>=0 && newX<w && newY >=0 && newY<h) {
                         const n_index = (newY*w + newX)*4
                         const n_a = data[n_index+3]
                         if(n_a == 0) {
@@ -59,6 +90,20 @@ class BorderIdentifyingUtil {
                 }
             }
         }
+    }
+}
+class AnimationHandler {
+    constructor(component) {
+        this.component = component
+    }
+    start() {
+        const interval = setInterval(()=>{
+            this.component.render()
+            this.component.update()
+            if(this.component.stopped() == true) {
+                clearInterval(interval)
+            }
+        },50)
     }
 }
 customElements.define('border-fill',BorderFillComponent)
