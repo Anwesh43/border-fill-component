215ba9bdae2bc8f9811a0e377990d5bb33a1acf1
Anwesh43 <anweshthecool0@gmail.com>
2017-06-13 16:58:59 +0530

    identifying border

--- border-fill-component.js
+++ border-fill-component.js
@@ -15,6 +15,9 @@ class BorderFillComponent extends HTMLElement{
         context.drawImage(this.image,this.image.width*0.1,this.image.height*0.1)
         if(!this.points && !this.index) {
             this.index = 0
+            this.points = []
+            BorderIdentifyingUtil.getBorder(context,this.points,w,h)
+            console.log(this.points)
         }
         this.img.src = canvas.toDataURL()
     }
@@ -26,3 +29,36 @@ class BorderFillComponent extends HTMLElement{
         }
     }
 }
+class BorderIdentifyingUtil {
+    static getBorder(context,points,w,h) {
+        const imgData = context.getImageData(0,0,w,h)
+        const data = imgData.data
+        console.log(data.length)
+        var opaquePoints = 0
+        for(var i=0;i<data.length;i+=4) {
+            const alpha = data[i+3]
+            const j = i/4
+            const x = j%w,y = Math.floor(i/w)
+            if(alpha == 0) {
+                //console.log(0)
+                opaquePoints++
+                continue
+            }
+
+            for(var a = -1;a<=1;a++) {
+                for(var b = -1;b<=1;b++) {
+                    const newX = x+b,newY = y+a
+                    if(newX>=0 && newX<w && newX >=0 && newY<h) {
+                        const n_index = (newY*w + newX)*4
+                        const n_a = data[n_index+3]
+                        if(n_a == 0) {
+                            points.push({x,y})
+                            break
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+customElements.define('border-fill',BorderFillComponent)
